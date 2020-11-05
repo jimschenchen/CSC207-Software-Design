8939823d8989ea4d063e2bc2dc9e339d42ccb5ea
dengzey2 <zeyun.deng@mail.utoronto.ca>
2020-11-04 21:12:02 -0500

    Class AttendeeMenu Class Main Class LogIn

---
+++ phase1/Presenter/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame welcome = new Welcome();
+        //JFrame login = new LogIn();
+        //JFrame am = new AttendeeMenu();
+    }
+}
