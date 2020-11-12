848bce1b1aa6fc95958f3258cf39a6d462171b94
jimschenchen <jimschenchen@163.com>
2020-11-12 17:38:48 +0800

    Hide Test method of `Gateway`

--- phase1/Gateway/Gateway.java
+++ phase1/Gateway/Gateway.java
@@ -20,29 +20,29 @@ public class Gateway {
         Utils.push(database);
     }
 
-    public static void main(String[] args) throws IOException {
-        // init
-        Gateway gw = new Gateway();
-
-        // read from file
-        DataBase db = gw.init();
-
-        // print after reading from file
-        System.out.println(db.getUserList());
-        db.getUserList().forEach((u) -> System.out.println(u.getUser_name()));
-        System.out.println(db.getNextRoomId());
-
-        // manipulate db
-        List<User> lst = new ArrayList<>();
-        User u1 = new Attendee(1, "Ji321312m", "Jim");
-        User u2 = new Attendee(2, "J1231", "jim2");
-        Organizer o1 = new Organizer(3, "31231231", "jim3");
-        lst.add(u1);
-        lst.add(u2);
-        lst.add(o1);
-
-
-         // save db
-         gw.termination(db);
-    }
+//    public static void main(String[] args) throws IOException {
+//        // init
+//        Gateway gw = new Gateway();
+//
+//        // read from file
+//        DataBase db = gw.init();
+//
+//        // print after reading from file
+//        System.out.println(db.getUserList());
+//        db.getUserList().forEach((u) -> System.out.println(u.getUser_name()));
+//        System.out.println(db.getNextRoomId());
+//
+//        // manipulate db
+//        List<User> lst = new ArrayList<>();
+//        User u1 = new Attendee(1, "Ji321312m", "Jim");
+//        User u2 = new Attendee(2, "J1231", "jim2");
+//        Organizer o1 = new Organizer(3, "31231231", "jim3");
+//        lst.add(u1);
+//        lst.add(u2);
+//        lst.add(o1);
+//
+//
+//         // save db
+//         gw.termination(db);
+//    }
 }
