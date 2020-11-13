d49c542ee2ee7f86478d0b0290c7a395c182c1c8
Jinxuan Wang <jinxuan.wang@mail.utoronto.ca>
2020-11-12 22:19:21 -0800

    done

--- phase1/Use Cases/EventManager.java
+++ phase1/Use Cases/EventManager.java
@@ -26,7 +26,7 @@ public class EventManager {
     public boolean canCreateEvent(int room_id, Double start, DataBase db){
         List<Event> all_event = db.getEventList();
         for (int i = 0; i < all_event.size(); i++){
-            if(room_id == all_event.get(i).getEvent_id() && start == all_event.get(i).getStart_time()){
+            if(room_id == all_event.get(i).getRoomId() && start == all_event.get(i).getStart_time()){
                 return false;
             }
         }
