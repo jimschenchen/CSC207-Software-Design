90189f48e3c16c4bd8d1241ddecc110b5d491085
jimschenchen <jimschenchen@163.com>
2020-11-14 16:29:07 +0800

    Update SerUtils and add javadoc

--- phase1/Gateway/Gateway.java
+++ phase1/Gateway/Gateway.java
@@ -10,12 +10,25 @@ import java.util.List;
  * @create: 2020-11-09 21:58
  **/
 public class Gateway {
-
+    /**
+    * @Description: deserializing database from file and return the database
+    * @Param: []
+    * @return: DataBase
+    * @Author:
+    * @Date: 2020-11-14
+    */
     public DataBase init () throws IOException {
         DataBase db = SerUtils.pull();
         return db;
     }
 
+    /**
+    * @Description: Serializing database to file
+    * @Param: [database]
+    * @return: void
+    * @Author:
+    * @Date: 2020-11-14
+    */
     public void termination (DataBase database) throws IOException {
         SerUtils.push(database);
     }
