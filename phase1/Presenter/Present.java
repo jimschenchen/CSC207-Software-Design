1b83f5d9e096a8f94127ac62a16026015bb8ce84
dengzey2 <zeyun.deng@mail.utoronto.ca>
2020-11-15 13:06:20 +0800

    Part of the presenter

--- phase1/Presenter/Present.java
+++ phase1/Presenter/Present.java
@@ -2,6 +2,8 @@ public class Present {
     public static void main(String[] args) {
         ViewModel model = new ViewModel();
         Presenter p = new Presenter(model);
+        p.Introduction();
+        p.IsUserRegistered();
 
 
     }
