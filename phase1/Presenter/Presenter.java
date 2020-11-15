9cb21166e1e587771db30539c43efe674410612d
Jinxuan Wang <jinxuan.wang@mail.utoronto.ca>
2020-11-14 22:30:45 -0800

    done

--- phase1/Presenter/Presenter.java
+++ phase1/Presenter/Presenter.java
@@ -58,6 +58,7 @@ public class Presenter {
             String pass = l[1];
             this.username = username;
             this.pass = pass;
+            this.id = cs.getUserIDbyUserName(username);
             EverythingCorrect();
         }
     }
