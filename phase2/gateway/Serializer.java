530fd53ee662f57765e351c90f558f8157a76f95
Chenyue <chenyue.li@mail.utoronto.ca>
2020-11-27 18:06:32 +0800

    Create New Gateway Connect to Redis DB; Add All `nextId` getter; Add Gson Serializer in the Gateway;

---
+++ phase2/gateway/Serializer.java
@@ -0,0 +1,14 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.lang.reflect.Type;
+
+/**
+ * @program: group_0173
+ * @description:
+ * @author:
+ * @create: 2020-11-27 11:17
+ **/
+public class Serializer {
+
+}
