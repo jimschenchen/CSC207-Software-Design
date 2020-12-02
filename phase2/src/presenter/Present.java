8c5454afe551149c47cfdcc9b323e6600b066fef
dengzey2 <zeyun.deng@mail.utoronto.ca>
2020-12-02 18:48:40 +0800

    Panel

--- phase2/src/presenter/Present.java
+++ phase2/src/presenter/Present.java
@@ -2,10 +2,12 @@ package presenter;
 
 import presenter.language.English;
 
+import javax.swing.*;
+
 public class Present {
     public static void main(String[] args) {
         //LoginWindow loginWindow = new LoginWindow();
         //SignUpWindow signUpWindow = new SignUpWindow();
-        LanguageWindow languageWindow = new LanguageWindow(new English());
+        //LanguageWindow languageWindow = new LanguageWindow(new English())Panel
     }
 }
