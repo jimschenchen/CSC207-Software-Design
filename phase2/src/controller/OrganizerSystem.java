a70c93a3707863d6f1ad40442d4852250da65b73
Jinxuan Wang <jinxuan.wang@mail.utoronto.ca>
2020-12-02 20:13:07 -0800

    small changes

--- phase2/src/controller/OrganizerSystem.java
+++ phase2/src/controller/OrganizerSystem.java
@@ -7,21 +7,5 @@ import entity.VipUser;
 import gateway.Gateway;
 
 public class OrganizerSystem {
-    public Organizer org;
 
-    public Organizer getOrg() {
-        return org;
-    }
-
-    public Speaker CreateSpeaker(String password, String name, Gateway gw) {
-        return new Speaker(gw.getNextUserId(), password, name);
-    }
-
-    public Attendee CreateAttendee(String password, String name, Gateway gw) {
-        return new Attendee(gw.getNextUserId(), password, name);
-    }
-
-    public VipUser CreateVIP(String password, String name, Gateway gw) {
-        return new VipUser(gw.getNextUserId(), password, name);
-    }
 }
