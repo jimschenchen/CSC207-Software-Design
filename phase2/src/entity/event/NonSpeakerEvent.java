68d60e067992ca2c76228e8da3e19add27ae476a
Jinxuan Wang <jinxuan.wang@mail.utoronto.ca>
2020-12-03 22:09:40 -0800

    import Nullable

--- phase2/src/entity/event/NonSpeakerEvent.java
+++ phase2/src/entity/event/NonSpeakerEvent.java
@@ -1,7 +1,7 @@
 package entity.event;
 
-import com.sun.istack.internal.Nullable;
-//import org.jetbrains.annotations.Nullabl e;
+//import com.sun.istack.internal.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
--- phase2/src/usecase/EventManager.java
+++ phase2/src/usecase/EventManager.java
@@ -6,11 +6,12 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.sun.istack.internal.Nullable;
+//import com.sun.istack.internal.Nullable;
 import entity.*;
 import entity.event.*;
 import entity.eventFactory.FactoryProducer;
 import gateway.GatewayFacade;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * The Event Manager class
