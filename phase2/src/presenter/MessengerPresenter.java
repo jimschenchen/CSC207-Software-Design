ea6cc628d967b8e648327d6cb6e68d62a470e1f0
shenliha <lihang.shen@mail.utoronto.ca>
2020-12-05 12:01:35 +0800

    12/4

--- phase2/src/presenter/MessengerPresenter.java
+++ phase2/src/presenter/MessengerPresenter.java
@@ -10,19 +10,19 @@ public class MessengerPresenter {
     ConferenceSystem msgSystem = new ConferenceSystem();
     IMessage msgWindow = new MessengerWindow();
 
-    void MessageAllAttendees(String content, String eventID){
+    void MessageAllAttendees(String title, String content, String eventID){
         //eventID//
         //How are we supposed to ask for an id?
         // I remember now users can message only those who are from the same event
         //Right now I am asking the user, but please change it if it doesn't fit anymore
-        boolean success = msgSystem.messageAllAttendeesInEvent(eventID, content);
+        boolean success = msgSystem.messageAllAttendeesInEvent(eventID, title, content);
         //Does presenter have an access to gateway?
         msgWindow.messageSuccess(success);
 
     }
 
-    void MessageOneAttendee(String content, String userID){
-        boolean success = msgSystem.messageAttendee(content, userID);
+    void MessageOneAttendee(String title, String content, String userID){
+        boolean success = msgSystem.messageAttendee(title, content, userID);
         msgWindow.messageSuccess(success);
         //They are not public. How do I get access to them?
     }
@@ -34,18 +34,18 @@ public class MessengerPresenter {
     List<String>  SeeSentMessages(){
         return msgSystem.readSentMessages();
     }
-    void MessageAllSpeakers(String content){
-        boolean success = msgSystem.messageAllSpeakers(content);
+    void MessageAllSpeakers(String title, String content){
+        boolean success = msgSystem.messageAllSpeakers(title, content);
         msgWindow.messageSuccess(success);
     }
 
-    void MessageOneSpeaker(String content, String speakerID){
-        boolean success = msgSystem.messageSpeaker(speakerID, content);
+    void MessageOneSpeaker(String title, String content, String speakerID){
+        boolean success = msgSystem.messageSpeaker(speakerID, title, content);
         msgWindow.messageSuccess(success);
     }
 
-    void MessageAllAttendeesOfSpeakerEvent(String content){
-        boolean success = msgSystem.messageAllUsersInAllSpeakingEvents(content);
+    void MessageAllAttendeesOfSpeakerEvent(String title, String content){
+        boolean success = msgSystem.messageAllUsersInAllSpeakingEvents(title, content);
         msgWindow.messageSuccess(success);
     }
 
