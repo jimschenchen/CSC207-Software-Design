cfd5062f4f64c15f5254051e1c10214cb8ad24ab
dengzey2 <zeyun.deng@mail.utoronto.ca>
2020-12-06 16:47:54 +0800

    Simplified Chinese

--- phase2/src/presenter/language/SimplifiedChinese.java
+++ phase2/src/presenter/language/SimplifiedChinese.java
@@ -62,8 +62,125 @@ public class SimplifiedChinese implements Language {
     public String messageTitle() {
         return ("消息");
     }
+
     @Override
     public String createAccountMessage() {
         return ("亲！你成功创建了一个账户呢！");
+    }
+
+    @Override
+    public String succeedAddRoomMessage(){
+        return ("创建房间成功");
+    }
+    @Override
+    public String successful(){
+        return ("成功！");
+    }
+    @Override
+    public String failed(){
+        return ("失败");
+    }
+    @Override
+    public String failAddRoomMessage(){
+        return("创建房间失败");
+    }
+    @Override
+    public String succeedCancelEventMessage(){
+        return("取消事件成功");
+    }
+
+    @Override
+    public String failCancelEventMessage(){
+        return("取消事件失败");
+    }
+
+    @Override
+    public String succeedSignUpEventMessage(){
+        return("报名事件成功");
+    }
+
+    @Override
+    public String failSignUpEvent(){
+        return("报名事件失败");
+    }
+
+    @Override
+    public String succeedWaitEvent(){
+        return("成功加入等待名单");
+    }
+
+    @Override
+    public String failWaitEventMessage(){
+        return("加入等待名单失败");
+    }
+
+    @Override
+    public String succeedChangeEventMessage(){
+        return("成功修改事件");
+    }
+
+    @Override
+    public String failChangeEventMessage(){
+        return("修改事件失败");
+    }
+
+    @Override
+    public String succeedCreateUserMessage(){
+        return("成功创建用户");
+    }
+
+    @Override
+    public String failCreateUserMessage(){
+        return("创建用户失败");
+    }
+
+    @Override
+    public String succeedCreateEventMessage(){
+        return("成功创建事件");
+    }
+
+    @Override
+    public String failCreateEventMessage(){
+        return("创建事件失败");
+    }
+
+    @Override
+    public String succeedCancelEnrollmentMessage(){
+        return("成功取消事件资格");
+    }
+
+    @Override
+    public String failCancelEnrollmentMessage(){
+        return("取消事件资格失败");
+    }
+
+    @Override
+    public String succeedRemoveWaitMessage(){
+        return("成功退出等待名单");
+    }
+
+    @Override
+    public String failRemoveWaitMessage(){
+        return("退出等待名单失败");
+    }
+
+    @Override
+    public String succeedResetPasswordMessage(){
+        return("成功设置新密码");
+    }
+
+    @Override
+    public String failResetPasswordMessage(){
+        return("设置新密码失败");
+    }
+
+    @Override
+    public String succeedCreateAccountMessage(){
+        return("成功创建账户");
+    }
+
+    @Override
+    public String failCreateAccount(){
+        return("创建账户失败");
     }
 }
