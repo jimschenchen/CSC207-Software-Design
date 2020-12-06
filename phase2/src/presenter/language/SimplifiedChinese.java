958a5835ed5a06e2273f8156cbf5154aa7c443d3
tangzhen <zhenhalfpi.tang@mail.utoronto.ca>
2020-12-06 06:44:25 -0500

    complete SimplifiedChinese

--- phase2/src/presenter/language/SimplifiedChinese.java
+++ phase2/src/presenter/language/SimplifiedChinese.java
@@ -183,4 +183,237 @@ public class SimplifiedChinese implements Language {
     public String failCreateAccount(){
         return("创建账户失败");
     }
+
+    @Override
+    public String Welcome(){
+        return ("欢迎");
+    }
+    @Override
+    public String myProfile(){
+        return ("个人资料");
+    }
+    @Override
+    public String logOut(){
+        return ("登出");
+    }
+    @Override
+    public String resetPass(){
+        return ("重置密码");
+    }
+    @Override
+    public String schedule(){
+        return ("日程表");
+    }
+    @Override
+    public String signUp(){
+        return ("活动报名");
+    }
+
+    @Override
+    public String signUpWait(){
+        return ("加入活动的等待列表");
+    }
+    @Override
+    public String signUpNowait(){
+        return ("Don't have to wait!");
+    }
+    @Override
+    public String viewMyEvent(){
+        return ("我的所有活动");
+    }
+    @Override
+    public String  viewMyWait(){
+        return ("我的等待列表");
+    }
+    @Override
+    public String  viewMyNoWait(){
+        return ("我的非等待列表");
+    }
+    @Override
+    public String  viewAllEvent(){
+        return ("查看所有活动");
+    }
+    @Override
+    public String  messenger(){
+        return ("消息系统");
+    }
+    @Override
+    public String  newPass(){
+        return ("新密码");
+    }
+    @Override
+    public String  eventInfo(){
+        return ("活动信息");
+    }
+    @Override
+    public String  withID(){
+        return (",ID 为 ");
+    }
+    @Override
+    public String  startAt(){
+        return (" 开始于 ");
+    }
+    @Override
+    public String  endAt(){
+        return (" 结束于 ");
+    }
+    @Override
+    public String  takePlace(){
+        return (" 举办在 ");
+    }
+    @Override
+    public String  whichIs(){
+        return (" which is ");
+    }
+
+    @Override
+    public String  title(){
+        return ("标题: ");
+    }
+    @Override
+    public String  duration(){
+        return ("时长: ");
+    }
+    @Override
+    public String  startTime(){
+        return ("开始时间: ");
+    }
+    @Override
+    public String  endTime(){
+        return ("结束时间: ");
+    }
+    @Override
+    public String  vIPStatus(){
+        return ("是否为VIP活动: ");
+    }
+    @Override
+    public String  waitinglist(){
+        return ("等待列表: ");
+    }
+    @Override
+    public String  signUpQuestion(){
+        return ("报名?");
+    }
+    @Override
+    public String  yes(){
+        return ("是!");
+    }
+    @Override
+    public String  enterRoomNum(){
+        return ("输入你想新建的房间号码: ");
+    }
+    @Override
+    public String  enterRoomCapacity(){
+        return ("输入新房间能容纳的最大人数:");
+    }
+    @Override
+    public String  create(){
+        return ("创建");
+    }
+    @Override
+    public String  eventType(){
+        return ("活动类型: ");
+    }
+    @Override
+    public String  party(){
+        return ("Party");
+    }
+    @Override
+    public String  talk(){
+        return ("Talk");
+    }
+    @Override
+    public String  discussion(){
+        return ("Panel Discussion");
+    }
+    @Override
+    public String  topic(){
+        return ("标题: ");
+    }
+    @Override
+    public String  roomNum(){
+        return ("房间号: ");
+    }
+    @Override
+    public String  capacity(){
+        return ("容量y: ");
+    }
+    @Override
+    public String  no(){
+        return ("否!");
+    }
+    @Override
+    public String  selectSpeaker(){
+        return ("选择演讲者: ");
+    }
+    @Override
+    public String  empty(){
+        return ("Empty");
+    }
+
+    @Override
+    public String  userType(){
+        return ("用户类型: ");
+    }
+    @Override
+    public String  speaker(){
+        return ("演讲者");
+    }
+    @Override
+    public String  organizer(){
+        return ("举办者");
+    }
+    @Override
+    public String  attendee(){
+        return ("参加者");
+    }
+    @Override
+    public String  cancel(){
+        return ("取消");
+    }
+    @Override
+    public String  rank(){
+        return ("排名: ");
+    }
+    @Override
+    public String  reallyQuestion(){
+        return ("真的确定么??");
+    }
+    @Override
+    public String  change(){
+        return ("改动: ");
+    }
+    @Override
+    public String  newWord(){
+        return ("活动新设定: ");
+    }
+    @Override
+    public String  viewTalks(){
+        return ("查看我的演讲: ");
+    }
+    @Override
+    public String  newUser(){
+        return ("新的用户");
+    }
+    @Override
+    public String  newRoom(){
+        return ("新的房间");
+    }
+    @Override
+    public String  newEvent(){
+        return ("新活动");
+    }
+    @Override
+    public String  organizedEvent(){
+        return ("举办的活动");
+    }
+    @Override
+    public String  changeEventSetting(){
+        return ("改变活动设定");
+    }
+    @Override
+    public String viewOrganizedEvent(){
+        return ("查看举办的活动");
+    }
+
 }
