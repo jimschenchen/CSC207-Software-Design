fc754d227b6a8a076263881a3f0bc50763ad4e2a
gr4ce <wygrace.li@mail.utoronto.ca>
2020-12-06 16:44:51 +0800

    add TraditionalChinese

--- phase2/src/presenter/language/TraditionalChinese.java
+++ phase2/src/presenter/language/TraditionalChinese.java
@@ -55,16 +55,136 @@ public class TraditionalChinese implements Language {
 
     @Override
     public String fail() {
-        return null;
+        return "糟糕！請重試一次！";
     }
 
     @Override
     public String messageTitle() {
-        return null;
+        return "訊息";
     }
 
     @Override
     public String createAccountMessage() {
-        return null;
+        return "成功註冊新帳號";
+    }
+
+    @Override
+    public String succeedAddRoomMessage() {
+        return "成功增加活動場地";
+    }
+
+    @Override
+    public String successful() {
+        return "成功";
+    }
+
+    @Override
+    public String failed() {
+        return "失敗";
+    }
+
+    @Override
+    public String failAddRoomMessage() {
+        return "活動場地已存在，或輸入有誤";
+    }
+
+    @Override
+    public String succeedCancelEventMessage() {
+        return "成功取消活動";
+    }
+
+    @Override
+    public String failCancelEventMessage() {
+        return "你沒有權限取消活動，或輸入有誤";
+    }
+
+    @Override
+    public String succeedSignUpEventMessage() {
+        return "成功報名";
+    }
+
+    @Override
+    public String failSignUpEvent() {
+        return "報名失敗，或輸入有誤";
+    }
+
+    @Override
+    public String succeedWaitEvent() {
+        return "成功加入活動等候名單";
+    }
+
+    @Override
+    public String failWaitEventMessage() {
+        return "加入活動等候名單失敗，或輸入有誤";
+    }
+
+    @Override
+    public String succeedChangeEventMessage() {
+        return "成功更改活動詳情";
+    }
+
+    @Override
+    public String failChangeEventMessage() {
+        return "更改活動詳情失敗，或輸入有誤";
+    }
+
+    @Override
+    public String succeedCreateUserMessage() {
+        return "成功新增賬戶";
+    }
+
+    @Override
+    public String failCreateUserMessage() {
+        return "新增賬戶失敗，或輸入有誤";
+    }
+
+    @Override
+    public String succeedCreateEventMessage() {
+        return "成功增加活動";
+    }
+
+    @Override
+    public String failCreateEventMessage() {
+        return "增加活動失敗，或輸入有誤";
+    }
+
+    @Override
+    public String succeedCancelEnrollmentMessage() {
+        return "成功退出活動";
+    }
+
+    @Override
+    public String failCancelEnrollmentMessage() {
+        return "退出活動失敗，或輸入有誤";
+    }
+
+    @Override
+    public String succeedRemoveWaitMessage() {
+        return "成功離開等候名單";
+    }
+
+    @Override
+    public String failRemoveWaitMessage() {
+        return "離開等候名單失敗，或輸入有誤";
+    }
+
+    @Override
+    public String succeedResetPasswordMessage() {
+        return "成功重設密碼";
+    }
+
+    @Override
+    public String failResetPasswordMessage() {
+        return "重設密碼失敗，或輸入有誤";
+    }
+
+    @Override
+    public String succeedCreateAccountMessage() {
+        return "註冊成功！";
+    }
+
+    @Override
+    public String failCreateAccount() {
+        return "註冊失敗，或輸入有誤";
     }
 }
